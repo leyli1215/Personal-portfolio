f01204efab8ceb8f6309ada2db1ec85d129d23b2
leyli1215 <68439175+leyli1215@users.noreply.github.com>
2024-12-11 03:35:52 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+document.getElementById("projectsLink").addEventListener("click", function() {
+  window.location.href = "projects.html";
+});
+ 
+
+ 
+
+document.getElementById("homeLink").addEventListener("click", function() {
+  window.location.href = "index.html";
+});
+
+document.getElementById("nasamore").addEventListener("click", function() {
+  window.location.href = "nasainfo.html";
+});
+
+document.getElementById("loopxmore").addEventListener("click", function() {
+  window.location.href = "loopxinfo.html";
+});
+
+document.getElementById("cybermore").addEventListener("click", function() {
+  window.location.href = "cyberinfo.html";
+});
+
+document.getElementById("passwordmore").addEventListener("click", function() {
+  window.location.href = "passwordinfo.html";
+});
+
+document.getElementById("tttmore").addEventListener("click", function() {
+  window.location.href = "tictactoeinfo.html";
+});
+document.getElementById("hmmore").addEventListener("click", function() {
+  window.location.href = "hangmaninfo.html";
+});
+
+document.getElementById("appmore").addEventListener("click", function() {
+  window.location.href = "iftheninfo.html";
+});
+document.getElementById("bjmore").addEventListener("click", function() {
+  window.location.href = "blackjackinfo.html";
+});
